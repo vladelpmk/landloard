502d833c77fa9f49c020cf2cc4d78558eaad96a7
Vladimir Gjorgievski <gorgievski.vladimir.l@gmail.com>
2023-09-07 15:04:38 +0200

    add g sheet support

--- src/config/retardapp-313cc34eea59.ts
+++ src/config/retardapp-313cc34eea59.ts
@@ -4,7 +4,7 @@ export default {
   type: "service_account",
   project_id: "retardapp",
   private_key_id: process.env.PRIVATE_KEY_ID,
-  private_key: process.env.PRIVATE_KEY,
+  private_key: process.env.PRIVATE_KEY.split(String.raw`\n`).join("\n"),
   client_email: process.env.CLIENT_EMAIL,
   client_id: process.env.CLIENT_ID,
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
