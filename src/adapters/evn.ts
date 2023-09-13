c2911f5718855f77896a7da29a51abfe4deec1fc
Vladimir Gjorgievski <gorgievski.vladimir.l@gmail.com>
2023-09-13 15:20:03 +0200

    get data

--- src/adapters/evn.ts
+++ src/adapters/evn.ts
@@ -1,38 +1,90 @@
+import  cron  from "node-cron";
+import { lunchConnectToBrowser, getOrAddPageByDomain } from "../controler/chrome.js";
+import { startAutoRefresh } from "../controler/page.js";
+import { debug } from "../lib/utils.js";
 import puppeteer from "puppeteer";
-import { Invoice } from "./types.js";
-import { input } from "../lib/browser.js";
-import { delay, debug } from "../lib/utils.js";
+import { addOrUpdateRow } from "./spreadsheets.js";
 
-//puppeteer.use(StealthPlugin());
+const domain = "https://www.evnonline.mk/";
 
-const loginUrl = "https://www.evnonline.mk/auth/login";
-const loggedInUrl = "https://www.evnonline.mk/authorized/home";
-const invoicedUrl = "https://www.evnonline.mk/authorized/downloadInvoices";
+const getData = async (page: puppeteer.Page) => {
+  debug("Get EVN data START");
 
-export const run = async (browser): Promise<Invoice[]> => {
-  debug(`open new page`);
-  const page = await browser.newPage();
+  const tableArray = await page.$eval("table.table-bordered tbody", (tbody) => {
+    const trs = Array.from(tbody.querySelectorAll("tr"));
+    return trs.map((tr) => {
+      let tds = Array.from(tr.querySelectorAll("td"));
+      return tds.map((td) => td.innerText);
+    });
+  });
 
-  debug(`go to page ${loginUrl}`);
-  await page.goto(loginUrl);
+  let result = []
 
-  await input(page, "UsernameEmail", process.env.EVN_USER);
-  await input(page, "password", process.env.EVN_PASS);
-  debug(`Click login`);
-  await page.click("button.buttonNext");
-  await delay(10000);
-
-  if ((await page.url()) !== loggedInUrl) {
-    let bodyHTML = await page.evaluate(() => document.body.innerHTML);
-    console.log(bodyHTML);
-    throw new Error(`Login failed ${await page.url()}`);
+  for (let i = 0; i < tableArray.length; i++) {
+    result.push({
+      amount: parseFloat(tableArray[i][5].replace(".", "").replace(",", ".")),
+      month: parseInt(tableArray[i][3].split("-")[1]).toString(),
+      year: parseInt(tableArray[i][3].split("-")[2]).toString(),
+      paid: tableArray[i][7] === "Платена",
+    });
   }
 
-  debug(`go to page ${invoicedUrl}`);
-  await page.goto(invoicedUrl);
-  await delay(1000);
+  for (let i = 0; i < result.length; i++) {
+    await addOrUpdateRow(result[i], "EVN");
+  }
 
-  browser.disconnect();
-  const result = [];
-  return result;
+  debug("Get EVN data END");
 };
+
+
+ 
+
+var task = cron.schedule(
+  "0 0 * * *",
+  (page) => {
+    debug("Run chon")
+    getData(page)
+  },
+  {
+    scheduled: false,
+  },
+);
+
+
+export const run = async () => {
+  try {
+    const browser = await lunchConnectToBrowser();
+    const page = await getOrAddPageByDomain(domain, browser);
+    startAutoRefresh(page);
+    task.start(page);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+
+  // debug(`open new page`);
+  // const page = await browser.newPage();
+
+  // debug(`go to page ${loginUrl}`);
+  // await page.goto(loginUrl);
+
+  // await input(page, "UsernameEmail", process.env.EVN_USER);
+  // await input(page, "password", process.env.EVN_PASS);
+  // debug(`Click login`);
+  // await page.click("button.buttonNext");
+  // await delay(10000);
+
+  // if ((await page.url()) !== loggedInUrl) {
+  //   let bodyHTML = await page.evaluate(() => document.body.innerHTML);
+  //   console.log(bodyHTML);
+  //   throw new Error(`Login failed ${await page.url()}`);
+  // }
+
+  // debug(`go to page ${invoicedUrl}`);
+  // await page.goto(invoicedUrl);
+  // await delay(1000);
+
+  // browser.disconnect();
+  // const result = [];
+  // return result;
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,5 @@
-import { run as runVodovod } from "./adapters/vodovod.js";
 import { run as runEvn } from "./adapters/evn.js";
 
 import "./adapters/spreadsheets.js";
-import { addOrUpdateRow } from "./adapters/spreadsheets.js";
-import puppeteer from "puppeteer";
-import { debug } from "./lib/utils.js";
-import {
-  lunch,
-  getOrAddPageByDomain,
-  lunchConnectToBrowser,
-} from "./controler/chrome.js";
-import { startAutoRefresh } from "./controler/page.js";
 
-// (async () => {
-//   const result = await runVodovod();
-//   result.map((invoice) => {
-//     addOrUpdateRow(invoice, "Vodovod");
-//   });
-// })();
-
-//
-const browser = await lunchConnectToBrowser();
-const page = await getOrAddPageByDomain("https://www.evnonline.mk/", browser);
-
-startAutoRefresh(page);
+runEvn()
